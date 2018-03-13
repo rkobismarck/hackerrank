afc3b77e4e52eef4439612dad0ec3aaa8686a14c
rkobismarck <rko.bismarck@gmail.com>
2018-03-12 23:05:50 -0600

    Added solution.
    Added solution for grading students problem at hacker rank. Fix < 3

--- algorithms/implementation/grading_students.js
+++ algorithms/implementation/grading_students.js
@@ -1,11 +1,10 @@
 let gradingStudtens = (array) =>{
-
   let modulo                = (e) => {let temp = (e % 5); let number = 0; temp > 0 ? number = Math.abs(temp - 5): number = 0; return number}
-  let calculateCalification = (e) => (e > 38) ? e + modulo(e) : e;
-  let processArray          = (argument, fn) => argument.map(e => (fn(e) - e) > 3 ? parseInt(fn(e)) : parseInt(e)); 
-
-  return processArray(array,calculateCalification)
+  let calculateCalification = (e) => (e >= 38) ? e + modulo(e) : e;
+  let calculateDiference 	= (e) => calculateCalification(e) - e;  
+  let processArray          = (argument) => argument.map(e => (calculateDiference(e) < 3) ? calculateCalification(e): e); 
+  return processArray(array);
 }
 
-let array_test = [75,67,40,33];
+let array_test = [73,67,38,33];
 console.log(gradingStudtens(array_test))
